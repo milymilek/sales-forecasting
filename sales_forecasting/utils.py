f383cef2a528af9f91626d93ae71b40fd1cd80a0
milymilek <milosz1596@wp.pl>
2024-10-26 16:53:22 +0200

    wip

--- sales_forecasting/utils.py
+++ sales_forecasting/utils.py
@@ -1,16 +1,41 @@
+from typing import Generator
+
 import numpy as np
 import pandas as pd
 
 
-def timeseries_split(df: pd.DataFrame, col: str, continuous: bool = True) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def timeseries_split(df: pd.DataFrame, max_month: int, col: str, continuous: bool = True) -> tuple[pd.DataFrame, pd.DataFrame]:
     assert df[col].dtype == np.int64, "Expected np.int64 timeseries month column"
-    max_month = df[col].max()
-    train_index = df[col] <= max_month - 2
-    valid_index = df[col] == max_month - 1
+    train_index = df[col] < max_month
     test_index = df[col] == max_month
 
     if continuous:
-        valid_index = train_index | valid_index
         test_index = train_index | test_index
 
-    return df[train_index], df[valid_index], df[test_index]
+    return df[train_index], df[test_index]
+
+
+def kfold_timeseries_split(
+    df: pd.DataFrame, col: str, k_min: int = 1, k_max: int = -1
+) -> Generator[tuple[pd.DataFrame, pd.DataFrame], None, None]:
+    if k_max == -1:
+        k_max = df[col].max()
+
+    for i in range(k_min, k_max + 1):
+        yield timeseries_split(df, i, col, continuous=False)
+
+
+def aggregate_monthly_sales_sum() -> pd.DataFrame: ...
+
+
+def build_submission_df(evaluation_df: pd.DataFrame, save_path: None | str = None) -> pd.DataFrame:
+    assert all(_ in list(evaluation_df.columns) for _ in ("shop_id", "item_id", "item_cnt_month"))
+
+    df_test_raw = pd.read_csv(".data/test.csv")
+    submission_cols = ["ID", "item_cnt_month"]
+    submission = df_test_raw.merge(evaluation_df, on=["shop_id", "item_id"])[submission_cols]
+
+    if save_path is not None:
+        submission.to_csv(save_path, index=False)
+
+    return submission
